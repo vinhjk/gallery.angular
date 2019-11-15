9167347c95a0fb3ead9842fb069d958c645ddc05
NguyenVinh <nguyenquangvinh2518@gmail.com>
2019-11-15 09:16:24 +0700

    gallery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {GalleryConfig} from './image-gallery/token';
+import {ImageGalleryModule} from './image-gallery/image-gallery.module';
 
 @NgModule({
   declarations: [
@@ -10,9 +10,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ImageGalleryModule,
   ],
-  providers: [],
+  providers: [
+    {provide: GalleryConfig, useValue: 3}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
